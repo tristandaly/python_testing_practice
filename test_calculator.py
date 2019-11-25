7185183313ffc2376530989c82ea5a310c1c1b4c
tristandaly <tristandaly@gmail.com>
2019-11-25 13:23:58 -0600

    Add float test pass

--- test_calculator.py
+++ test_calculator.py
@@ -6,5 +6,8 @@ class TestCalculator(unittest.TestCase):
     def test_add_integer(self):
         self.assertEqual(calculator.add(2, 3), 5)
 
+    def test_add_float(self):
+        self.assertEqual(calculator.add(2.2, 44.4), 46.6)    
+
 if __name__ == '__main__':
     unittest.main()
