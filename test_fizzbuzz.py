e3b788afd7a143e793fbe582c3d1363a5c7303a0
tristandaly <tristandaly@gmail.com>
2019-11-25 11:44:07 -0600

    Buzz Tests ok

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,5 +6,8 @@ class TestFizzBuzz(unittest.TestCase):
     def test_multiple_of_three(self):
         self.assertEqual(fizzbuzz.process(6), 'Fizz')
 
+    def test_multiple_of_five(self):
+        self.assertEqual(fizzbuzz.process(20), 'Buzz')    
+
 if __name__ == '__main__':
-    unittest.main()        
+    unittest.main()
