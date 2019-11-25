2989a5f9e885d72835d669071f71d64dfe3712aa
tristandaly <tristandaly@gmail.com>
2019-11-25 13:25:52 -0600

    Add string test pass

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,10 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.add(2, 3), 5)
 
     def test_add_float(self):
-        self.assertEqual(calculator.add(2.2, 44.4), 46.6)    
+        self.assertEqual(calculator.add(2.2, 44.4), 46.6)
+
+    def test_add_string(self):
+        self.assertEqual(calculator.add("Butt ", "Head"), "Butt Head")        
 
 if __name__ == '__main__':
     unittest.main()
