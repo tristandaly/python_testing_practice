57e89734f33c06d69915c5e28e53b29d061ed005
tristandaly <tristandaly@gmail.com>
2019-11-26 14:19:18 -0600

    Add comments

--- test_calculator.py
+++ test_calculator.py
@@ -2,30 +2,30 @@ import calculator
 import unittest
 
 class TestCalculator(unittest.TestCase):
-
+    # Addition
     def test_add_integer(self):
         self.assertEqual(calculator.add(2, 3), 5)
 
     def test_add_float(self):
         self.assertEqual(calculator.add(2.2, 44.4), 46.6)
-
+    # Subtraction
     def test_subtract_integer(self):
         self.assertEqual(calculator.subtract(9, 5), 4)
 
     def test_subtract_float(self):
         self.assertAlmostEqual(calculator.subtract(99.9, 88.8), 11.1)
-
+    # Multiplication
     def test_multiply_integer(self):
         self.assertEqual(calculator.multiply(5, 5), 25)
 
     def test_multiply_float(self):
         self.assertEqual(calculator.multiply(30.9, 77.5), 2394.75)
-
+    # Division
     def test_divide_integer(self):
         self.assertEqual(calculator.divide(10, 5), 2)
 
     def test_divide_integer_with_remainder(self):
-        self.assertEqual(calculator.divide(10, 6), str(1.4))            
+        self.assertEqual(calculator.divide(10, 6), str(1.4))
 
 if __name__ == '__main__':
     unittest.main()
